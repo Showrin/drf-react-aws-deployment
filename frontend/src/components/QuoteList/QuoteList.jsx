cf82166555514204a8d130c2e687216fa442c1de
Showrin <showrinece15@gmail.com>
2023-04-23 05:20:51 +0600

    :sparkles: Fetch all quotes in the frontend

--- frontend/src/components/QuoteList/QuoteList.jsx
+++ frontend/src/components/QuoteList/QuoteList.jsx
@@ -1,34 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Section from "../Section";
 import Row from "../Row";
 import Col from "../Col";
 import QuoteCard from "../QuoteCard";
+import { apiClient } from "../../config/apiClient";
 
 const QuoteList = () => {
+  const [quotes, setQuotes] = useState([]);
+
+  const fetchQuotes = () => {
+    apiClient.get("/quotes/").then((response) => setQuotes(response.data));
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
   return (
     <Section>
       <Row>
-        <Col xl={3} lg={2} md={1}>
-          <QuoteCard content="A son asked his father (a programmer) why the sun rises in the east, and sets in the west. His response? It works, don't touch!" />
-        </Col>
-        <Col xl={3} lg={2} md={1}>
-          <QuoteCard content="A son asked his father (a programmer) why the sun rises in the east, and sets in the west. His response? It works, don't touch!" />
-        </Col>
-        <Col xl={3} lg={2} md={1}>
-          <QuoteCard content="A son asked his father (a programmer) why the sun rises in the east, and sets in the west. His response? It works, don't touch!" />
-        </Col>
-        <Col xl={3} lg={2} md={1}>
-          <QuoteCard content="A son asked his father (a programmer) why the sun rises in the east, and sets in the west. His response? It works, don't touch!" />
-        </Col>
-        <Col xl={3} lg={2} md={1}>
-          <QuoteCard content="A son asked his father (a programmer) why the sun rises in the east, and sets in the west. His response? It works, don't touch!" />
-        </Col>
-        <Col xl={3} lg={2} md={1}>
-          <QuoteCard content="A son asked his father (a programmer) why the sun rises in the east, and sets in the west. His response? It works, don't touch!" />
-        </Col>
-        <Col xl={3} lg={2} md={1}>
-          <QuoteCard content="A son asked his father (a programmer) why the sun rises in the east, and sets in the west. His response? It works, don't touch!" />
-        </Col>
+        {quotes.map((quote) => (
+          <Col xl={3} lg={2} md={1}>
+            <QuoteCard content={quote.quote} />
+          </Col>
+        ))}
       </Row>
     </Section>
   );
