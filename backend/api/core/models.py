9e2c95db50424d441aad43d30122efef01a898a0
Showrin <showrinece15@gmail.com>
2023-04-22 12:50:15 +0600

    :card_file_box: Create Quote model and migrations for it

--- backend/api/core/models.py
+++ backend/api/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Quotes(models.Model):
+    quote = models.TextField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
