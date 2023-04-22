2f048a12a12943eddf047336ec817e59a09fc2d9
Showrin <showrinece15@gmail.com>
2023-04-23 05:15:09 +0600

    :sparkles: Fetch random quote in the frontend

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
-const Button = ({ children, type }) => {
+const Button = ({ children, type, onClick }) => {
   return (
     <button
       className={classNames("button", {
         [`button--${type}`]: type,
       })}
+      onClick={onClick}
     >
       {children}
     </button>
@@ -17,10 +18,12 @@ const Button = ({ children, type }) => {
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   type: PropTypes.oneOf(["primary", "secondary"]),
+  onClick: PropTypes.func,
 };
 
 Button.defaultProps = {
   type: "primary",
+  onClick: () => {},
 };
 
 export default Button;
--- frontend/src/components/FeaturedQuoteSection/FeaturedQuoteSection.jsx
+++ frontend/src/components/FeaturedQuoteSection/FeaturedQuoteSection.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Section from "../Section";
 import Button from "../Button";
+import { apiClient } from "../../config/apiClient";
 
 const FeaturedQuoteSection = () => {
+  const [quote, setQuote] = useState("");
+
+  const fetchRandomQuote = () => {
+    apiClient
+      .get("/quotes/random/")
+      .then((response) => setQuote(response.data.quote));
+  };
+
+  useEffect(() => {
+    fetchRandomQuote();
+  }, []);
+
   return (
     <Section className="featured-quote-section">
       <h1 className="featured-quote-section__title">
         Enjoy your cookie with a funny Quote
       </h1>
-      <div className="featured-quote-section__quote">
-        “A son asked his father (a programmer) why the sun rises in the east,
-        and sets in the west. His response? It works, don't touch!”
-      </div>
-      <Button type="secondary">Get another Quote</Button>
+      <div className="featured-quote-section__quote">“{quote}”</div>
+      <Button type="secondary" onClick={fetchRandomQuote}>
+        Get another Quote
+      </Button>
     </Section>
   );
 };
