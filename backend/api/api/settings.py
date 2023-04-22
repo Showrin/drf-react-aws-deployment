8582a1c247d954fa981ad8f72599671d68cf8822
Showrin <showrinece15@gmail.com>
2023-04-22 11:22:18 +0600

    :card_file_box: Setup postgres db for backend

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('POSTGRES_DB_NAME'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
     }
 }
 
