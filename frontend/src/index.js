32d2519d2d4f39f0dfbcf0a0e53f3c126f6511f5
Showrin <showrinece15@gmail.com>
2023-04-22 15:05:21 +0600

    :lipstick: Add reset css in the frontend app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+import "./app.css";
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
