2c8d4657b637a677f12026b60259b4d47653f273
Showrin <showrinece15@gmail.com>
2023-04-22 23:43:24 +0600

    :truck: Move logo file to component/assets folder

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactComponent as Logo } from "./logo.svg";
+import { ReactComponent as Logo } from "../assets/logo.svg";
 import Button from "../Button";
 import Container from "../Container";
 
