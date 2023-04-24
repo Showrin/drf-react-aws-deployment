0b8de1bf365e4f6581ee2f66513bdf6e0e05f914
Showrin <showrinece15@gmail.com>
2023-04-24 22:11:39 +0600

    :sparkles: feat (Navbar, Button): Add github star button in the navbar

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -2,26 +2,39 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
-const Button = ({ children, type, onClick }) => {
-  return (
-    <button
-      className={classNames("button", {
-        [`button--${type}`]: type,
-      })}
-      onClick={onClick}
-    >
+const Button = ({ className, children, type, onClick, href, target }) => {
+  const btnClasses = classNames(
+    "button",
+    {
+      [`button--${type}`]: type,
+    },
+    className
+  );
+
+  return href ? (
+    <a className={btnClasses} href={href} target={target}>
+      {children}
+    </a>
+  ) : (
+    <button className={btnClasses} onClick={onClick}>
       {children}
     </button>
   );
 };
 
 Button.propTypes = {
+  className: PropTypes.string,
+  href: PropTypes.string,
+  target: PropTypes.string,
   children: PropTypes.node.isRequired,
-  type: PropTypes.oneOf(["primary", "secondary"]),
+  type: PropTypes.oneOf(["primary", "secondary", "ghost"]),
   onClick: PropTypes.func,
 };
 
 Button.defaultProps = {
+  className: "",
+  href: null,
+  target: "_self",
   type: "primary",
   onClick: () => {},
 };
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as Logo } from "../assets/logo.svg";
+import { ReactComponent as Github } from "../assets/github.svg";
 import Button from "../Button";
 import Container from "../Container";
 
@@ -10,7 +11,15 @@ const Navbar = () => {
         <span className="header__brand">
           <Logo className="header__logo" /> Funkie
         </span>
-        <Button>Post a Quote</Button>
+        <Button
+          className="header__github-button"
+          href="https://github.com/Showrin/drf-react-aws-deployment"
+          target="_blank"
+          type="ghost"
+        >
+          <Github />
+          Give a Star
+        </Button>
       </Container>
     </header>
   );
